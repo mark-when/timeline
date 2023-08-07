a537b8f32a6a9fc89b4273d7ffd7057d00d4f4ee
Rob Koch <kochrt@gmail.com>
2023-08-07 11:03:42 -0400

    update the now line periodically

--- src/Timeline/Events/NowLine.vue
+++ src/Timeline/Events/NowLine.vue
@@ -1,18 +1,29 @@
 <script setup lang="ts">
+import { onMounted, ref } from "vue";
 import { DateTime } from "luxon";
 import { useTimelineStore } from "../timelineStore";
 
 const timelineStore = useTimelineStore();
-const now = DateTime.now();
+const now = ref(DateTime.now());
+
+const setNow = () => {
+  now.value = DateTime.now();
+  setTimeout(() => setNow(), 1000 * 60);
+};
+
+onMounted(() => {
+  setNow();
+});
 </script>
 
 <template>
   <div
     v-if="!timelineStore.hideNowLine"
     class="absolute dark:bg-slate-400 bg-blue-300"
-    :style="`width: 1px; left: ${timelineStore.distanceFromBaselineLeftmostDate(
-      now
-    ) + timelineStore.leftInsetWidth}px; height: max(100vh, 100%);`"
+    :style="`width: 1px; left: ${
+      timelineStore.distanceFromBaselineLeftmostDate(now) +
+      timelineStore.leftInsetWidth
+    }px; height: max(100vh, 100%);`"
   ></div>
 </template>
 
