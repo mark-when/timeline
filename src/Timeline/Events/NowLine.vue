e182a450064afd3268f22d2e07828ff1c440a7d4
Rob Koch <kochrt@gmail.com>
2023-09-26 13:09:37 -0400

    make sure the now line doesn't get too far away from us

--- src/Timeline/Events/NowLine.vue
+++ src/Timeline/Events/NowLine.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 import { DateTime } from "luxon";
 import { useTimelineStore } from "../timelineStore";
 import { useNodeStore } from "../useNodeStore";
@@ -16,15 +16,20 @@ const setNow = () => {
 onMounted(() => {
   setNow();
 });
+
+const left = computed(() => {
+  return Math.min(
+    Math.max(0, timelineStore.distanceFromBaselineLeftmostDate(now.value)),
+    timelineStore.pageSettings.viewport.width * 6
+  );
+});
 </script>
 
 <template>
   <div
     v-if="!timelineStore.hideNowLine"
     class="absolute dark:bg-slate-400 bg-blue-300 top-0 bottom-0"
-    :style="`width: 1px; left: ${timelineStore.distanceFromBaselineLeftmostDate(
-      now
-    )}px; height: max(${nodeStore.viewHeight}, 100%);`"
+    :style="`width: 1px; left: ${left}px; height: max(${nodeStore.viewHeight}, 100%);`"
   ></div>
 </template>
 
