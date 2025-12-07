9bbfe96a1208cdd2cb894fe6da08fe990ca09053
Rob Koch <kochrt@gmail.com>
2025-12-07 09:51:18 -0800

    encoding and decoding path components for directories

--- src/router/router.ts
+++ src/router/router.ts
@@ -5,7 +5,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/:user?/:timeline?",
+      path: "/:user?/:timeline(.*)?",
       name: "home",
       component: App,
     },
